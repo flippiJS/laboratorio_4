c8ca8bad1ba9acfff51a17a8d77ea3e92cce43a5
alumno <alumno@utnfra.com.ar>
2019-08-22 20:12:11 -0300

    subida inicial

---
+++ project-labo4/src/app/componentes/bienvenido/bienvenido.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenido',
+  templateUrl: './bienvenido.component.html',
+  styleUrls: ['./bienvenido.component.scss']
+})
+export class BienvenidoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
