23797c73e890b422e8226d95eb093dd19fec2919
alumno <alumno@utnfra.com.ar>
2019-08-22 20:26:22 -0300

    bienvenido + login

--- project-labo4/src/app/app-routing.module.ts
+++ project-labo4/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'inicio',
+  component: BienvenidoComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project-labo4/src/app/app.module.ts
+++ project-labo4/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BienvenidoComponent
+    BienvenidoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
