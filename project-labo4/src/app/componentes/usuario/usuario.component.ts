970fa121398e784330d52a571f738e018521f4a2
alumno <alumno@utnfra.com.ar>
2019-08-29 20:51:26 -0300

    Formulario usuario

--- project-labo4/src/app/componentes/usuario/usuario.component.ts
+++ project-labo4/src/app/componentes/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/clases/usuario';
 
 @Component({
   selector: 'app-usuario',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.component.scss']
 })
 export class UsuarioComponent implements OnInit {
+  ListadoDeUsuarios: Array<Usuario> = new Array<Usuario>();
+  UsuarioForm = {usuario: null, clave: null, clave2: null};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  AgregarUsuario(){
+    const usuarioNuevo = new Usuario(this.UsuarioForm.usuario, this.UsuarioForm.clave);
+    this.ListadoDeUsuarios.push(usuarioNuevo);
   }
 
 }
